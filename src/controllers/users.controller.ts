920f11386dc5850196aed92eb3e46a7762f42888
marllonwagner <marllonwagner91@gmail.com>
2023-04-26 10:49:26 -0300

    req 07 go horse mode

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,14 +2,9 @@ import { Request, Response } from 'express';
 import UserService from '../services/user.service';
 
 const insertUser = async (req: Request, res: Response) => {
-  const result = await UserService.insertUser(req.body);
-  return res.status(201).json(result);
+  const { statusCode, response } = await UserService.insertUser(req.body);
+  return res.status(statusCode).json(response);
 };
-
-// const getAllProducts = async (req: Request, res: Response) => {
-//   const result = await ProductService.getAllProducts();
-//   return res.status(200).json(result);
-// };
 
 export default {
   insertUser,
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,14 +3,22 @@ import { User } from '../interfaces/users.interface';
 import { generateToken } from '../auth/jwt';
 
 const insertUser = async (insertedUser: User) => {
+  if (!insertedUser.username) {
+    return { statusCode: 400, response: { message: '"username" is required' } };
+  }
+  if (!insertedUser.level) {
+    return { statusCode: 400, response: { message: '"level" is required' } };
+  }
+  if (!insertedUser.password) {
+    return { statusCode: 400, response: { message: '"password" is required' } };
+  }
+  if (!insertedUser.vocation) {
+    return { statusCode: 400, response: { message: '"vocation" is required' } };
+  }
   const id = await UserModel.insertUser(insertedUser);
   const token = generateToken({ ...insertedUser, id });
-  return { token };
+  return { statusCode: 201, response: { token } };
 };
-// const getAllProducts = async () => {
-//   const [result] = await ProductModel.getAllProducts();
-//   return result;
-// };
 export default {
   insertUser,
 };
